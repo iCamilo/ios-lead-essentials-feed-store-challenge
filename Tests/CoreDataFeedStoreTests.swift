0da53622080fe2fcabb999e131e7ecdb7c5aa1bc
Iván Fuertes <ivancamilofuerte>
2020-06-25 19:57:52 -0500

    Delete delivers No Error On Empty Cache

--- Tests/CoreDataFeedStoreTests.swift
+++ Tests/CoreDataFeedStoreTests.swift
@@ -49,9 +49,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
 
     func test_delete_deliversNoErrorOnEmptyCache() {
-//        let sut = makeSUT()
-//
-//        assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
+        let sut = makeSUT()
+
+        assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
     }
 
     func test_delete_hasNoSideEffectsOnEmptyCache() {
